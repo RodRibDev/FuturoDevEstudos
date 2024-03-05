f25ac766f46a741bcaf757d2108bbef12487047e
RodRibDev <rodrigo_r_ribeiro@estudante.sesisenai.org.br>
2024-03-04 22:56:25 -0300

    Execução exercício 04

---
+++ modulo1/semana06/ex04.js
@@ -0,0 +1,15 @@
+function cadastrarUsuario() {
+    var nome = prompt("Digite seu nome: ");
+    var idade = prompt("Digite sua idade: ");
+    var email = prompt("Digite seu email: ");
+
+    var usuario = {
+        nome: nome,
+        idade: idade,
+        email: email
+    };
+
+    localStorage.setItem('user', JSON.stringify(usuario));
+}
+
+cadastrarUsuario();
